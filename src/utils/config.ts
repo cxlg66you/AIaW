da3e1e6e4375f0cf318e2e41d33f25d4ad8a2bf1
cxlg66you <cxlg66you@gmail.com>
2025-01-03 13:13:53 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,6 @@ export const DocParseBaseURL = process.env.DOC_PARSE_BASE_URL
 export const CorsFetchBaseURL = process.env.CORS_FETCH_BASE_URL
 export const SyncServicePrice = process.env.SYNC_SERVICE_PRICE && parseFloat(process.env.SYNC_SERVICE_PRICE)
 export const UsdToCnyRate = process.env.USD_TO_CNY_RATE && parseFloat(process.env.USD_TO_CNY_RATE)
-export const DexieDBURL = process.env.DEXIE_DB_URL
+export const DexieDBURL = 'https://zz9bhaju9.dexie.cloud'
 export const LitellmBaseURL = process.env.LITELLM_BASE_URL
 export const BudgetBaseURL = process.env.BUDGET_BASE_URL
